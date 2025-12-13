955379fbcd7a9d7e6d9c1f44635513299de95c18
ramesh mishra <ramesh.a.mishra@capgemini.com>
2025-12-13 15:36:03 +0530

    initial

---
+++ CI_CDDemo/CI_CDDemo/CI_CDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CDDemoApp.swift
+//  CI_CDDemo
+//
+//  Created by Ramesh Kumar Mishra  on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CI_CDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CI_CDDemo/CI_CDDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CI_CDDemo
+//
+//  Created by Ramesh Kumar Mishra  on 13/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
